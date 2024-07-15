50e78525f4c5eab88a3baa24b0d98629ff6f0153
devdoni <devdoni1005@gmail.com>
2024-07-16 00:23:28 +0900

    0715 main_thum fix & sub_thum fix DH

--- src/project/Home.jsx
+++ src/project/Home.jsx
@@ -1,18 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Menubar from "./Menubar";
 import './css/home.css';
 import CategorySlideView from "./CategorySlideView";
 import { Link, useLocation } from "react-router-dom";
 import Slider from "react-slick";
+import MainGames from './db/MainGames.json';
 
 
 const Home = () => {
+
+    //Hook 
+    const [games, setGames] = useState([]);
+    useEffect(() => {
+        const getData = () => {
+            console.log('MainGames==>', MainGames)
+            setGames(MainGames);
+            console.log('games==>', games)
+        };
+        getData();
+    }, []);
+
+    // 슬라이드를 설정하는 부분
     const settings = {
         dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
+        infinite: true,
+        autoplay:true,
+        autoplaySpeed: 4000,
+        speed: 700,
+        slidesToShow: 3,
+        slidesToScroll: 3
+        
       };
 
     return(
@@ -21,70 +38,111 @@ const Home = () => {
 
         <div id="mainthum_slide">
             <Slider {...settings} className="main_slider">
-                <div className="main_thum">
-                    <Link to={'/detail/01'}>
-                        <img src={process.env.PUBLIC_URL + '/imgs/mainimgs/palworld.jpg'} alt="Palworld" className="mimgs"/>
+                {games.map((game) => (
+                <div id="main_content">
+                <div className="main_thum" key={game.no}>
+                    <Link to={`/${game.href}`}>
+                        <img src={`./imgs/${game.src}`} alt={`${game.name}`} className="mimgs"/>
+                        <div className="overlay">
+                            <h3>{game.name}</h3>
+                            <p>{game.info}</p>
+                        </div>
                     </Link>
                 </div>
-                <div className="main_thum">
-                    <img src={process.env.PUBLIC_URL + '/imgs/mainimgs/stardewvalley.jpg'} alt="Stardew Valley" className="mimgs"/>
                 </div>
-                <div className="main_thum">
-                    <img src={process.env.PUBLIC_URL + '/imgs/mainimgs/callofduty.jpg'} alt="Call of Duty" className="mimgs"/>
-                </div>
+            ))}
             </Slider>
+
         </div>
 
-
-                {/* <ul className="mainimg">
-                    <li><img src={process.env.PUBLIC_URL + './imgs/leftarrow.png'} className="arw" /></li>
-                    <li><Link to={'/detail/:01'}><img src={process.env.PUBLIC_URL + './imgs/mainimgs/palworld.jpg'} className="mimgs"/></Link></li>
-                    <li><img src={process.env.PUBLIC_URL + './imgs/mainimgs/stardewvalley.jpg'} className="mimgs"/></li>
-                    <li><img src={process.env.PUBLIC_URL + './imgs/mainimgs/callofduty.jpg'} className="mimgs"/></li>
-                    <li><img src={process.env.PUBLIC_URL + './imgs/rightarrow.png'} className="arw" /></li>
-                </ul>
-
-                <div className="btnbox" />
-                <div className="btnbox" />
-                <div className="btnbox" />
-                <div className="btnbox" />
-                <div className="btnbox" />
-                <div className="btnbox" />
-                <div className="btnbox" /> */}
-
-                
-                    <div className="table1">
+                    <div className="game_table">
                         <table>
-                            <tr className="thead1">
+                            <tr className="thead">
                                 <td colSpan="2"><h2>할인/가격</h2>
                                 <p>집중 조명 태그</p></td>
                             </tr>
                             <div className="tablebgimg">
                             <tr>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/battlegrounds.jpg'} className="table1imgs"/></td>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/alyx.jpg'} className="table1imgs"/></td>
+                                <td>
+                                    <div className="img_container">
+                                        <img src={process.env.PUBLIC_URL + './imgs/mainimgs/battlegrounds.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>        
+                                </td>
+                                <td>
+                                    <div className="img_container">
+                                        <img src={process.env.PUBLIC_URL + './imgs/mainimgs/alyx.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
                             </tr>
                             <tr>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/sekiro.jpg'} className="table1imgs"/></td>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/pahasmophobia.jpg'} className="table1imgs"/></td>
+                                <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/sekiro.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/pahasmophobia.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
                             </tr>
                             </div>
                         </table>
                         
 
                         <table>
-                            <tr className="thead1">
+                            <tr className="thead">
                                 <td colSpan="2"><h2>무료제품</h2>
                                 <p>집중 조명 태그</p></td>
                             </tr>
                             <div className="tablebgimg">
                             <tr>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/sekiro.jpg'} className="table1imgs"/></td>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/spider-man_remastered.jpg'} className="table1imgs"/></td>
+                                <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/sekiro.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/spider-man_remastered.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
                             </tr>
                             <tr>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/assassin`s_odyssey.jpg'} className="table1imgs"/></td>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/assassin`s_creed_varhalla.jpg'} className="table1imgs"/></td>
+                                <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/assassin`s_odyssey.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/assassin`s_creed_varhalla.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
                             </tr>
                             </div>
                         </table>
@@ -93,38 +151,95 @@ const Home = () => {
                     <button className="morebtn_left">더보기</button>
                     <button className="morebtn_right">더보기</button>
 
-                    <div className="table1">
+                    <div className="game_table">
                         <table>
-                            <tr className="thead1">
+                            <tr className="thead">
                                 <td colSpan="2"><h2>가상 현실게임</h2>
                                 <p>집중 조명 태그</p></td>
                             </tr>
                             <div className="tablebgimg">
                             <tr>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/cyberpunk2077.jpg'} className="table1imgs"/></td>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/hogwarts_legacy.jpg'} className="table1imgs"/></td>
+                                <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/cyberpunk2077.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/hogwarts_legacy.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                    </div>
+                                </td>
                             </tr>
                             <tr>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/war_thunder.jpg'} className="table1imgs"/></td>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/yu-gi-oh_master_duel.jpg'} className="table1imgs"/></td>
+                                <td>
+                                    <div className="img_container">
+                                        <img src={process.env.PUBLIC_URL + './imgs/mainimgs/war_thunder.jpg'} className="table1imgs"/>
+                                            <div className="overlay">
+                                                <p>게임 짧은소개</p>
+                                            </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className="img_container">
+                                        <img src={process.env.PUBLIC_URL + './imgs/mainimgs/yu-gi-oh_master_duel.jpg'} className="table1imgs"/>
+                                            <div className="overlay">
+                                                <p>게임 짧은소개</p>
+                                            </div>
+                                    </div>
+
+                                </td>
                             </tr>
                             </div>
                         </table>
                         
 
                         <table>
-                            <tr className="thead1">
+                            <tr className="thead">
                                 <td colSpan="2"><h2>가상 현실게임</h2>
                                 <p>집중 조명 태그</p></td>
                             </tr>
                             <div className="tablebgimg">
                             <tr>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/baldur`s_gate3.jpg'} className="table1imgs"/></td>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/sannabi.jpg'} className="table1imgs"/></td>
+                            <td>
+                                <div className="img_container">
+                                    <img src={process.env.PUBLIC_URL + './imgs/mainimgs/baldur`s_gate3.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="img_container">
+                                <img src={process.env.PUBLIC_URL + './imgs/mainimgs/sannabi.jpg'} className="table1imgs"/>  
+                                    <div className="overlay">
+                                        <p>게임 짧은소개</p>
+                                    </div>
+                                </div>
+                            </td>
                             </tr>
                             <tr>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/tekken.jpg'} className="table1imgs"/></td>
-                                <td><img src={process.env.PUBLIC_URL + './imgs/mainimgs/torchlight.jpg'} className="table1imgs"/></td>
+                            <td>
+                                <div className="img_container">
+                                    <img src={process.env.PUBLIC_URL + './imgs/mainimgs/tekken.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                </div>                               
+                            </td>
+                            <td>
+                                <div className="img_container">
+                                    <img src={process.env.PUBLIC_URL + './imgs/mainimgs/torchlight.jpg'} className="table1imgs"/>
+                                        <div className="overlay">
+                                            <p>게임 짧은소개</p>
+                                        </div>
+                                </div>
+                            </td>
                             </tr>
                             </div>
                         </table>
@@ -137,43 +252,7 @@ const Home = () => {
                     <div className="categorybox">
                         <h2>카테고리별로 살펴보기</h2>
                         <CategorySlideView />
-                        {/* <img src="./imgs/leftarrow.png" className="arw"/>
-                        <a className="rpgimg">
-                            <div className="imgcarousel">
-                                <img src="./imgs/category/bladur`s_gate3_rpg.jpg"  className="imgcsl"/>
-                                <div className="gradient"></div>
-                                <span>RPG</span>
-                            </div>
-                        </a>
-
-                        <a className="smulimg">
-                            <div className="imgcarousel">
-                                <img src="./imgs/category/stardew_valley_simulation.jpg"  className="imgcsl"/>
-                                <div className="gradient"></div>
-                                <span>시뮬레이션</span>
-                            </div>
-                        </a>
-
-                        <a className="sptimg">
-                            <div className="imgcarousel">
-                                <img src="./imgs/category/football_manager2024_allsports.jpg"  className="imgcsl"/>
-                                <div className="gradient"></div>
-                                <span>스포츠</span>
-                            </div>
-                        </a>
-                        <img src="./imgs/rightarrow.png" className="arw"/> */}
-
                     </div>
-
-                    {/* 
-
-                        <div className="btnbox" />
-                        <div className="btnbox" />
-                        <div className="btnbox" />
-
-                        
-                        
-                    </div> */}
                     
             </div>
         </>
