45612bd0ce5fa5f86dd734d3c0cc4feb82d2746e
stjdbsdk1 <stjdbsdk1@gmail.com>
2024-07-17 17:22:34 +0900

    0717 s1

--- src/project/review/ReviewList.jsx
+++ src/project/review/ReviewList.jsx
@@ -1,33 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 import '../css/review.css'
+import { getUserReviewDB } from '../utils/utils';
 
-const ReviewList = ({ reviews }) => {
+
+
+
+const ReviewList = ({ gameName, writeFlag }) => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        console.log('[ReviewList] useEffect()');
+        setData(); 
+    },[writeFlag, gameName]);
+    
+
+
+    const setData = () => {
+        const userReviewDB = getUserReviewDB();
+
+        if (!userReviewDB) {
+            // 데이터베이스가 없는 경우 처리
+            setReviews([]);
+            return;
+        }
+
+        const allReviews = [];
+
+        for (const user in userReviewDB) {
+            if (userReviewDB.hasOwnProperty(user)) {
+                const userData = userReviewDB[user];
+
+                if (userData.hasOwnProperty(gameName)) {
+                    const reviewData = userData[gameName];
+                    allReviews.push({
+                        user: user,
+                        gameName: gameName,
+                        nick: reviewData.nick,
+                        star: reviewData.star,
+                        review: reviewData.review,
+                        regDate: reviewData.regDate
+                    });
+                }
+            }
+        }
+
+        setReviews(allReviews);
+    };
     return (
         <div id="review_list">
-            {reviews.map(review => (
-                <div key={review.id} className="review_item">
-                    <div className="review_header">
-                    <div className="nickname">
-                            {review.uNick}
+            {reviews.length === 0 ? (
+                <div className="no-reviews">리뷰가 없습니다.</div>
+            ) : (
+                reviews.map((review, idx) => (
+                    <div key={idx} className="review_item">
+                        <div className="review_header">
+                            <div className="nickname">{review.nick}</div>
+                            <div className="rating">별점: {review.star}</div>
                         </div>
-                        <div id="rating">
-                            {Array(5).fill(0).map((_, index) => (
-                                <FaStar
-                                    key={index}
-                                    size="20"
-                                />
-                            ))}
+                        <div className="review_text">
+                            <p>{review.review}</p>
+                            <p className="reg-date">작성일시: {review.regDate}</p>
                         </div>
                     </div>
-                    <div className="review_text">
-                        {review.text}
-                    </div>
-                </div>
-            ))}
+                ))
+            )}
         </div>
     );
 };
 
 export default ReviewList;
-
