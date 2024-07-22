9e0f4fb1c70c0a0c7c3860be73b1d1dcaa77b5be
devdoni <devdoni1005@gmail.com>
2024-07-22 17:51:50 +0900

    free game fix

--- src/project/games/Free.jsx
+++ src/project/games/Free.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from 'framer-motion'
-import "../css/games.css";
-import "../css/home.css";
+import '../css/free.css'
 import FreeCustomArrow from "../etc/FreeCustomArrow";
 import Slider from "react-slick";
 import MainGames from '../db/MainGames.json';
 import popularDB from '../db/popularDB.json';
+import MainCustomArrow from "../etc/MainCustomArrow";
 
 
 
@@ -25,17 +25,31 @@ const Free = () => {
 
 const settings = {
     dots: true,
-    dotsClass: 'free-slick-dots custom-dots',
     infinite: false,
     autoplay:true,
     autoplaySpeed: 4000,
     speed: 700,
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow: <FreeCustomArrow icon="./imgs/rightarrow.png" className={"free-slick-next"}/>,
-    prevArrow: <FreeCustomArrow icon="./imgs/leftarrow.png" className={"free-slick-prev"}/>
-    
-
+    centerMode: true, // 중앙 정렬 모드 활성화
+    centerPadding: '0px', // 중앙 정렬 시 여백을 0으로 설정
+    nextArrow: <MainCustomArrow icon="./imgs/rightarrow.png" className="custom-arrow" />,
+    prevArrow: <MainCustomArrow icon="./imgs/leftarrow.png" className="custom-arrow" />,
+    appendDots: dots => (
+        <div style={{ bottom: "10px" }}>
+            <ul style={{ margin: "0px" }}>{dots}</ul>
+        </div>
+    ),
+    customPaging: i => (
+        <button
+            style={{
+                width: "10px",
+                height: "10px",
+                borderRadius: "50%",
+                background: "rgba(255, 255, 255, 0.5)"
+            }}
+        />
+    )
   };
 
   const [popularArr, setPopularArr] = useState([]);
@@ -54,65 +68,59 @@ const settings = {
 
     
 
-return(
-    <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 1 }}
-  >
-    <div id="hompage">
-
-        <div id="freethum_slide">
-            <h1>무료 플레이 게임</h1>
-            <Slider {...settings} className="free_slider">
-                {games.map((game) => (
-                <div id="free_content">
-                <div className="free_thum" key={game.no}>
-                    <Link to={`/${game.href}`}>
-                    <div className="free-image-info-container">
-                        <img src={`./imgs/${game.src}`} alt={`${game.name}`} className="fimgs"/>
-                        <div className="free-game_info">
-                            <h3>{game.name}</h3>
-                            <p>{game.info}</p>
-                        </div>
-                    </div>
-                    </Link>
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+        >
+            <div id="hompage">
+                <div id="freethum_slide">
+                    <h1>무료 플레이 게임</h1>
+                    <Slider {...settings} className="free_slider">
+                        {games.map((game) => (
+                            <div className="free_content" key={game.no}>
+                                <div className="free_thum">
+                                    <Link to={`/${game.href}`}>
+                                        <div className="free-image-info-container">
+                                            <img src={`./imgs/${game.src}`} alt={`${game.name}`} className="fimgs" />
+                                            <div className="free-game_info">
+                                                <h3>{game.name}</h3>
+                                                <p>{game.info}</p>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </Slider>
                 </div>
-                </div>
-            ))}
-            </Slider>
-        </div>
-
-        <div id="free_wrap">
-            <div className="free-header">
-                무료 게임
-            </div>
-        <div className="free-sub-header">
-            <div className="free-sub-header-item sub-item">게임 이름</div>
-        </div>
-        <div className="items">
-        {
-            popularArr.map((popular, idx) => {
-                return (
-                    <div className="item" key={idx}>
-                        <div className="rank">{idx + 1}</div>
-                        <div className="thumbnail">
-                            <Link to={`/detail/${popular['no']}`} onClick={linkClickHandler}>
-                                <img src={popular['thumnail-link']} alt={popular['title']} />
-                            </Link>
-                        </div>
-                        <div className="title">{popular['Name']}</div>
-                        <button className="free-play">무료 플레이</button>
-                    </div>
-                )
-            })
-        }
-    </div>
-</div>
-    </div>
-    </motion.div>
     
-
-    )
+                <div id="free_wrap">
+                    <div className="free-header">
+                        무료 게임
+                    </div>
+                    <div className="free-sub-header">
+                        <div className="free-sub-header-item sub-item">게임 이름</div>
+                    </div>
+                    <div className="items">
+                        {popularArr.map((popular, idx) => (
+                            <div className="item" key={idx}>
+                                <div className="rank">{idx + 1}</div>
+                                <div className="thumbnail">
+                                    <Link to={`/detail/${popular['no']}`} onClick={linkClickHandler}>
+                                        <img src={popular['thumnail-link']} alt={popular['title']} />
+                                    </Link>
+                                </div>
+                                <div className="title">{popular['Name']}</div>
+                                <button className="free-play">무료 플레이</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </motion.div>
+    );
 }
-export default Free;
+
+    export default Free;
